9a014bb6d2971dec95593ffec6863426ddf9baf7
hatn23 <nguyet.ha.tran23@gmail.com>
2020-03-26 10:11:41 +0100

    assembleur

--- assembleur.c
+++ assembleur.c
@@ -5,20 +5,11 @@
 
 instruction tab_instruction[MAX_TABLESIZE];
 int index_tab = 0;
+int index_execute = 0;
 
-void add_instruction2(char *operation, int r1, r2) {
+void add_instruction(int operation,int r1, int r2, int r3){
     struct instruction add;
-    add.operation = strdup(operation);
-    add.r1 = r1;
-    add.r2 = r2;
-    add.r3 = 0;
-    tab_instruction[index_tab] = add;
-    index_tab ++;
-}
-
-void add_instruction3(char *operation,int r1, int r2, int r3){
-    struct instruction add;
-    add.operation = strdup(operation);
+    add.operation = operation;
     add.r1 = r1;
     add.r2 = r2;
     add.r3 = r3;
@@ -30,11 +21,61 @@ int get_index_tab() {
     return index_tab;
 }
 
+int get_index_execute(){
+    return index_execute;
+}
+
 void interpreter(){
-    for (int i = 0;i++; i<line){
+    struct instruction instruction = tab_instruction[index_execute];
+    switch (instruction.type){
+        case ADD:
+            break;
+        case MUL:
+            break;
+        case SOU:
+            break;
+        case DIV:
+            break;
+        case COP:
+            break;
+        case AFC:
+            break;
+        case JMP:
+            break;
+        case JMF:
+            break;
+        case INF:
+            break;
+        case SUP:
+            break;
+        case EQU:
+            break;
+        case PRI:
+            break;
 
     }
+    
 
+}
+
+char *str_instructions[7] = {"COP", "AFC", "PRI", "ADD", "SOU", "MUL", "DIV"};
+
+            
+
+
+
+}
+
+void print_instruction(instruction i){
+    printf("Operation :   %s | Registre 1 : %d | Registre 2 : %d | Registre 3 : %d\n", i.operation, i.r1, i.r2, i.r3);
+}
+
+void print_all(){
+    for (int i = 0, i <= index_tab, i++){
+        printf("%d. ", i);
+        print_instruction(tab_instruction[i]);
+        printf("\n");
+    }
 }
 
 /*int main(){
